ba23748b53b3c3b8f0bc8dd90a12cc0117417f74
Jordan K <jkelley12@gmail.com>
2011-06-01 16:52:09 -0700

    Added a stub User Home Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 SampleApp::Application.routes.draw do
   
-  get "users/new"
+  resources :users
 
-    root :to => "pages#home"
+  root :to => "pages#home"
   
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
